fe63ef09f0f30d885bc768a264c9b797a976c04d
jeanchiang1221 <89722400+jeanchiang1221@users.noreply.github.com>
2022-10-01 18:08:15 +0800

    Add files via upload

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace EasySplitProject
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+           // config.EnableCors();
+            // Web API 設定和服務
+
+            //// Web API 路由
+            //config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+        }
+    }
+}
